89357abe65a985c70e46f331303506148691f17e
nilsjenn <93837106+nilsjenn@users.noreply.github.com>
2023-03-20 19:48:41 +0100

    Try's added.

--- chatgpt_docu.py
+++ chatgpt_docu.py
@@ -18,32 +18,39 @@ for i in range(number_paragraphs):
     docu_list.append(prompt)
 
 def gpt_docu(prompt):
-  print(f"----- {prompt} -----")
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "user", "content": prompt}
-    ]
-  )
+    try:
+      print(f"----- {prompt} -----")
+      completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          {"role": "user", "content": prompt}
+        ]
+      )
 
-  answer = completion.choices[0].message.content
-  for i in range(2):
-      try:
-          if answer[0] == "\n":
-              answer = answer[1:]
-      except:
-          pass
+      answer = completion.choices[0].message.content
+      for i in range(2):
+          try:
+              if answer[0] == "\n":
+                  answer = answer[1:]
+          except:
+              pass
 
-      print(f"Answer: {answer}")
-  return answer
+
+      return answer
+    except:
+      print("Something went wrong. Please try again.")
+
 
 # append all answers to a list
 answer_list = []
-
-for i in range(len(docu_list)):
-    print(f"\n----- Generating answer for: Question {i+1}/{len(docu_list)} -----")
-    answer = gpt_docu(docu_list[i])
-    answer_list.append(answer)
+try:
+    for i in range(len(docu_list)):
+        print(f"\n----- Generating answer for: Question {i+1}/{len(docu_list)} -----")
+        answer = gpt_docu(docu_list[i])
+        answer_list.append(answer)
+        print(f"----- Answer : {answer} -----\n")
+except:
+    print("Something went wrong. Please try again.")
 
 #%%
 # --- CREATE DOCUMENT ---
